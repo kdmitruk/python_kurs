837fe3b7669e8e030b4d5f3b5c0f5d874267bb8a
kdmitruk <kdmitruk@gmail.com>
2023-02-19 09:38:11 +0100

    krok 1

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __str__(self) -> str:
+        return f"Point({self.x}, {self.y})"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    p = Point()
+    p.x = 100
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(p)
+
+
+main()
