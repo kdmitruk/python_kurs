fa5a2aed459f556002f660cd632419c0c42bdb6e
kdmitruk <kdmitruk@gmail.com>
2023-02-19 10:27:39 +0100

    krok 3

--- main.py
+++ main.py
@@ -9,19 +9,29 @@ class Point:
     def __str__(self) -> str:
         return f"Point({self.x}, {self.y})"
 
+
 class Segment:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+
+    def __str__(self) -> str:
+        return f"Segment({self.p1}, {self.p2})"
+
     def distance(self):
-        return math.hypot(self.p1.x-self.p2.x, self.p1.y-self.p2.y)
+        return math.hypot(self.p1.x - self.p2.x, self.p1.y - self.p2.y)
+
+    def svg(self):
+        return f'<line x1="{self.p1.x}" y1="{self.p1.y}" x2="{self.p2.x}" y2="{self.p2.y}" style="stroke:black" />'
+
+
 def main():
-    p = Point(3,0)
-    q = Point(0,4)
-    f = Segment(p,q)
+    p = Point(300, 0)
+    q = Point(0, 400)
+    f = Segment(p, q)
+    print(f)
     print(f.distance())
-
-
+    print(f.svg())
 
 
 main()
