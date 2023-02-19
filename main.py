0d4c86368abfef4d841f8e0a1810c29b2434bebb
kdmitruk <kdmitruk@gmail.com>
2023-02-19 10:02:19 +0100

    krok 2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Point:
     def __init__(self, x=0, y=0):
         self.x = x
@@ -6,12 +9,19 @@ class Point:
     def __str__(self) -> str:
         return f"Point({self.x}, {self.y})"
 
-
+class Segment:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    def distance(self):
+        return math.hypot(self.p1.x-self.p2.x, self.p1.y-self.p2.y)
 def main():
-    p = Point()
-    p.x = 100
+    p = Point(3,0)
+    q = Point(0,4)
+    f = Segment(p,q)
+    print(f.distance())
 
-    print(p)
+
 
 
 main()
