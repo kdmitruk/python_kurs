8b1b083e981369e591fe9ffcc45d1d8ea5cc6f0b
kdmitruk <kdmitruk@gmail.com>
2023-03-05 10:33:03 +0100

    lab 2 krok 2

--- lab2.py
+++ lab2.py
@@ -7,13 +7,23 @@ def person_from_line(line : str):
     entry = line.split(",")
     return Person(entry[0],entry[1], datetime.datetime.strptime(entry[2],
                         "%Y-%m-%d"))
+
+def people_from_csv(path):
+    file = open(path, 'r')
+    people = []
+    for line in file.readlines():
+        people.append(person_from_line(line.rstrip()))
+    return people
+
 def main():
     # p = Person("Jan", "Kowalski", "1.2.1970")
     # p.last_name = "Nowak"
     # print(p.last_name)
-    person_str = "Kial,Toffic,1998-07-25"
-    p=person_from_line(person_str)
-    print(p)
+    # person_str = "Kial,Toffic,1998-07-25"
+    # p=person_from_line(person_str)
+    # print(p)
+    people = people_from_csv("people.csv")
+    print(people)
 
 if __name__ == "__main__":
     main()
