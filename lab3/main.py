3f18b8532ae829c3aceaba308673570339f56d2a
kdmitruk <kdmitruk@gmail.com>
2023-03-25 12:17:23 +0100

    lab 3 zad 4 kr 1

--- lab3/main.py
+++ lab3/main.py
@@ -1,15 +1,14 @@
 #import reverse_polish_notation as rpn
 import random
-
 from reverse_polish_notation import *
-
 from monte_carlo import approximate_pi
+import simple_draw as sd
 
 def main():
     #print(rvp("((2+7)/3+(8-3)*4)/2")) # 2 7 + 3 / 8 3 − 4 * + 2 /
     #print(solve("2 7 + 3 / 14 3 - 4 * + 2 /"))
-    print(approximate_pi())
-
+    #print(approximate_pi())
+    sd.draw_rect()
 
 if __name__ == "__main__":
     main()
---
+++ lab3/simple_draw.py
@@ -0,0 +1,18 @@
+import turtle
+
+def draw_rect():
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    t.speed(1)
+
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+
+    screen.exitonclick()
+
