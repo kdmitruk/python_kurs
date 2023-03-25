ddb6c642ceef7ceb072824686c6118a8e0f5330f
kdmitruk <kdmitruk@gmail.com>
2023-03-25 10:29:05 +0100

    lab 3 zad 1 kr 2

--- lab3/main.py
+++ lab3/main.py
@@ -1,9 +1,8 @@
+operators = ['+', '-', '*', '/', '(', ')']
 def rvp(infix):
     stack = []
     postfix = ""
-    operators = ['+', '-', '*', '/', '(', ')']
     priority = {'+':1, '-':1, '*':2, '/':2}
-
     for c in infix:
         if c not in operators:
             postfix += c
@@ -17,16 +16,30 @@ def rvp(infix):
             while len(stack)>0 and stack[-1]!='(' and priority[c]<=priority[stack[-1]]:
                 postfix += stack.pop()
             stack.append(c)
-
     while len(stack)>0:
         postfix += stack.pop()
-
     return postfix
 
-
-
+def solve(postfix):
+    stack=[]
+    for c in postfix.split():
+        if c not in operators:
+            stack.append(int(c))
+        else:
+            right = stack.pop()
+            left = stack.pop()
+            if c=="+":
+                stack.append(right+left)
+            if c=="-":
+                stack.append(left-right)
+            if c=="*":
+                stack.append(right*left)
+            if c=="/":
+                stack.append(left/right)
+    return stack.pop()
 def main():
-    print(rvp("((2+7)/3+(8−3)*4)/2")) # 2 7 + 3 / 8 3 − 4 * + 2 /
+    #print(rvp("((2+7)/3+(8−3)*4)/2")) # 2 7 + 3 / 8 3 − 4 * + 2 /
+    print(solve("2 7 + 3 / 14 3 - 4 * + 2 /"))
 
 if __name__ == "__main__":
     main()
