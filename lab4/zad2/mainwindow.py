351d3d29c6015e9ef8a38e4e0595ce7de2b27fda
kdmitruk <kdmitruk@gmail.com>
2023-03-26 11:31:03 +0200

    lab 4 zad 2 krok 3

--- lab4/zad2/mainwindow.py
+++ lab4/zad2/mainwindow.py
@@ -1,5 +1,6 @@
 from PySide2.QtWidgets import *
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -16,3 +17,15 @@ class MainWindow(QMainWindow):
         openAction = fileMenu.addAction("Open")
         saveAction = fileMenu.addAction("Save")
         saveAsAction = fileMenu.addAction("Save as")
+        newAction.triggered.connect(self.onNewAction)
+        openAction.triggered.connect(self.onOpenAction)
+
+    def onNewAction(self):
+        self.edit.clear()
+
+    def onOpenAction(self):
+        path, _ = QFileDialog.getOpenFileName(self, "Open file", "")
+        if len(path) > 0:
+            file = open(path, 'r')
+            data = file.read()
+            self.edit.setText(data)
