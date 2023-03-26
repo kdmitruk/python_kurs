d5c00d65a91312f4906b7fa215831dbc9abfa34b
kdmitruk <kdmitruk@gmail.com>
2023-03-26 12:04:00 +0200

    lab 4 zad 2 krok 4

--- lab4/zad2/mainwindow.py
+++ lab4/zad2/mainwindow.py
@@ -9,6 +9,7 @@ class MainWindow(QMainWindow):
 
         self.edit = QTextEdit(self)
         self.setCentralWidget(self.edit)
+        self.path = ""
 
     def initializeMenu(self):
         menuBar = self.menuBar()
@@ -19,9 +20,12 @@ class MainWindow(QMainWindow):
         saveAsAction = fileMenu.addAction("Save as")
         newAction.triggered.connect(self.onNewAction)
         openAction.triggered.connect(self.onOpenAction)
+        saveAction.triggered.connect(self.onSaveAction)
+        saveAsAction.triggered.connect(self.onSaveAsAction)
 
     def onNewAction(self):
         self.edit.clear()
+        self.path = ""
 
     def onOpenAction(self):
         path, _ = QFileDialog.getOpenFileName(self, "Open file", "")
@@ -29,3 +33,25 @@ class MainWindow(QMainWindow):
             file = open(path, 'r')
             data = file.read()
             self.edit.setText(data)
+            self.path = path
+            file.close()
+
+    def onSaveAsAction(self):
+        path,_ = QFileDialog.getSaveFileName(self, "Save file")
+        if len(path) > 0:
+            self.__save(path)
+
+    def onSaveAction(self):
+        if self.path == "":
+            self.onSaveAsAction()
+
+        else:
+            self.__save(self.path)
+
+    def __save(self,path):
+        file = open(path, "w")
+        data = self.edit.toPlainText()
+        file.write(data)
+        file.close()
+        self.path = path
+
