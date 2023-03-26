3123e9bd967fbd329e2a3fb881e05a1a84fce54c
kdmitruk <kdmitruk@gmail.com>
2023-03-26 13:18:43 +0200

    lab 4 zad 3 krok 2

--- lab4/zad3/mainwidget.py
+++ lab4/zad3/mainwidget.py
@@ -1,17 +1,35 @@
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from PySide2.QtCore import *
 
 class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(500, 500)
+        self.setFixedSize(800, 800)
+        self.playerPos = QPoint(self.width()/2,self.height()/2)
+
 
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
         brush = QBrush()
-        brush.setColor("white")
+        brush.setColor(QColor(231, 236, 163))
         brush.setStyle(Qt.SolidPattern)
         print(self.rect())
         painter.fillRect(self.rect(), brush)
+        brush.setColor(QColor(131, 36, 163))
+        painter.fillRect(QRect(self.playerPos.x()-10,self.playerPos.y()-10,20,20),brush)
+
         painter.end()
+
+    def keyPressEvent(self, event):
+        step=5
+        if event.key() == Qt.Key_Left:
+            self.playerPos.setX(self.playerPos.x()-step)
+        if event.key() == Qt.Key_Right:
+            self.playerPos.setX(self.playerPos.x()+step)
+        if event.key() == Qt.Key_Up:
+            self.playerPos.setY(self.playerPos.y()-step)
+        if event.key() == Qt.Key_Down:
+            self.playerPos.setY(self.playerPos.y()+step)
+        self.update()
